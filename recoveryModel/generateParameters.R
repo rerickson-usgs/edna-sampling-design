276a3e2e80a3615b356a6c5daf13f2a3135acb26
rerickson <rerickson@usgs.gov>
2017-06-22 14:49:53 -0500

    added generate parameters file to git

---
+++ recoveryModel/generateParameters.R
@@ -0,0 +1,24 @@
+library(data.table)
+
+## Generate all possible parameter combinations and place into table and then save
+K_in        <- c( 2, 4, 8, 16)
+theta_in    <- c( 0.06, 0.24, 0.42, 0.76)
+p_in        <- c( 0.15, 0.3, 0.35, 0.4, 0.75)
+psi_in      <- 1
+nSamples_in <- c( 5, 10, 20, 30, 50, 75, 100, 125)
+
+
+parameterValue <- data.table(expand.grid(
+    K = K_in,
+    theta = theta_in,
+    p = p_in,
+    psi = psi_in,
+    nSamples = nSamples_in))
+parameterValue[ , Index :=  1:dim(parameterValue)[1]]
+
+
+fileFolder = "./"
+
+write.csv(x = parameterValue, file = paste0("parmaterValue.csv"), row.names = FALSE)
+
+
